d7aff9bd470d50a25b0fd26dbc151d1182fe3b90
sohyun <kshyun0215@gmail.com>
2024-05-22 17:33:01 +0900

    fix: Svg Icon 속성 수정

--- src/components/NavIcon/HomeIcon.tsx
+++ src/components/NavIcon/HomeIcon.tsx
@@ -8,8 +8,8 @@ const HomeIcon = ({ className }: { className: string }) => (
     className={className}
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M12.54 2.05499L22.08 10.755L21.015 11.82L19.5 10.446V20.235L18.75 20.985H14.25L13.5 20.235V14.985H10.5V20.235L9.75 20.985H5.25L4.5 20.235V10.458L3 11.82L1.935 10.755L11.46 2.05499H12.54ZM6 9.09449V19.485H9V14.235L9.75 13.485H14.25L15 14.235V19.485H18V9.08549L12 3.64499L6 9.09449Z"
       fill="currentColor"
     />
